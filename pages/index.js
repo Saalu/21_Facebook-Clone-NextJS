1989f61c89bcdcefb224dacf78add99d6e0dc1bc
Saalu <saaluissak@gmail.com>
2021-07-05 11:10:44 +0000

    Header UI

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import Login from "../components/Login";
+import { getSession } from "next-auth/client";
+
+export default function Home({ session }) {
+  // if (!session) return <Login />;
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+      </Head>
+
+      <Header />
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+    </div>
+  );
+}
+
+// export async function getServerSideProps(context) {
+//   //Get the user
+//   const session = await getSession(context);
+
+//   return {
+//     props: {
+//       session,
+//     },
+//   };
+// }
